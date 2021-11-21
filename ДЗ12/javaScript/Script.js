8fe7503d26c11fea845ce03d33466b193329fc7f
Yurchelly <yurchelly@gmail.com>
2021-11-21 22:23:29 +0200

    homeWork12

---
+++ ДЗ12/javaScript/Script.js
@@ -0,0 +1,34 @@
+/* 
+ var a = 7;
+var b = 2;
+var ost = a % b;
+if (ost != 0) {
+    alert('Делится с остатком ' + ost);
+} else {
+    alert('Делится, результат деления: ' + (a / b));
+}
+*/
+
+/*
+function drawTriangle (a,x) {
+  for (let i = 0; i <= a; i++) {
+    for (let j = a - 1; j >= i; j--) {
+      console.log("x");}
+      }
+ */
+ 
+/*
+ for (var i = 1; i < 100; i += 2) {
+    console.log(i);
+}
+*/
+
+/*
+var i = 1000
+var num = 0
+while (i > 50)
+{ i = i / 2
+num++ }
+    console.log(i)
+console.log('количество итераций', num)
+*/
