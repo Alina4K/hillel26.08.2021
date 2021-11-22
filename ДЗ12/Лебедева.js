7a2aa96483bc2e8226f894cd1a9f0c01e258471c
Lebedieva2705 <lubashatsymbalist@gmail.com>
2021-11-22 19:14:04 +0200

    HomeWork10

---
+++ ДЗ12/Лебедева.js
@@ -0,0 +1,34 @@
+/*let a= 8;
+let b=3;
+let c= 8%3;
+if (c !=0) {
+	console.log('делится' + (a%b));}
+else if (c = 0) {
+console.log('Делится без остатка');}*/
+
+/*
+ Выведите столбец нечетных чисел в промежутке от 0 до 100.
+*/
+/*for (let i = 0; i<=100; x++) {
+	console.log (x % 2);
+	if(x % 2 =0) {continue;}
+}
+console.log('только нечетные: x=' + x);
+*/
+
+function drawTriangle (a,x) 
+{
+  for (let i = 0; i <= a; i++) {
+      console.log("x"); }
+}
+
+
+/*
+var i = 1000
+var e = 0
+while (i > 50)
+{ i = i / 2
+e++ }
+    console.log(i)
+console.log('количество итераций', e)
+*/
