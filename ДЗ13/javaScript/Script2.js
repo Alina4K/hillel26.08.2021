3214e9e89de6c891f028c26cf8fdd0ad19325c07
Yurchelly <yurchelly@gmail.com>
2021-11-28 15:34:25 +0200

    homeWork13

--- ДЗ13/javaScript/Script2.js
+++ ДЗ13/javaScript/Script2.js
@@ -3,17 +3,17 @@ function validate (){
 
 var fn = document.getElementById('fname')
 var fname = fn.value
-var searchResult = /^[a-zA-Z]{2,20}$/.test(fname)
+var searchResult = /[a-zA-Z]{2,20}/.test(fname)
 console.log(fname,searchResult)
 
 var em = document.getElementById('email')
 var email = em.value
-var searchResult1 = /^[a-zA-Z]{6,12}@(gmail\.com|ukr\.net)$/.test(email)
+var searchResult1 = /[a-zA-Z]{6,12}@(gmail\.com|ukr\.net)/.test(email)
 console.log(email,searchResult1)
 
 var ph = document.getElementById('phone')
 var phone = ph.value
-var searchResult2 = /^380(50|63|66|67|68|91|92|93|97|96)\d{7}$/.test(phone)
+var searchResult2 = /380(50|63|66|67|68|91|92|93|97|96)\d{7}/.test(phone)
 console.log(phone,searchResult2)
 
 if (searchResult && searchResult1 && searchResult2) {
