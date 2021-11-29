2256c7c4bf0a266ea95f9ae22f70d24d36ef36e2
Yurchelly <yurchelly@gmail.com>
2021-11-29 19:56:13 +0200

    homeWork13

--- ДЗ13/javaScript/Script2.js
+++ ДЗ13/javaScript/Script2.js
@@ -1,24 +1,31 @@
 
-function validate (){
-
-
-    var re = /[a-zA-Z]{2,20}/;
+function validate (fname){
+   
     var fname = document.getElementById('fname').value;
+    var re = /[a-zA-Z]{2,20}/;
     var valid = re.test(fname);
-   
-    var re =  /[a-zA-Z]{6,12}@(gmail\.com|ukr\.net)/;
-    var email = document.getElementById('email').value;
-    var valid = re.test(email);
-   
-
-    var re = /380(50|63|66|67|68|91|92|93|97|96)\d{7}/;
-    var phone = document.getElementById('phone').value;
-    var valid = re.test(phone);
-    
-    
-
-if (fname && email && phone)
-	alert("Мы услышим Вас");
+    if (re.test(fname) == true )
+    	alert("Мы услышим Вас");
     else 
-    alert("Мы вас не слышим");
+   alert("Мы вас не слышим");   
 }
+function validate (email){
+    var email = document.getElementById('email').value;
+    var re =  /[a-zA-Z]{6,12}@(gmail\.com|ukr\.net)/;
+    var valid = re.test(email);
+   if (re.test(email) == true )
+    	alert("Мы услышим Вас");
+    else 
+   alert("Мы вас не слышим");
+}
+
+function validate (phone){
+    var phone = document.getElementById('phone').value;
+    var re = /380(50|63|66|67|68|91|92|93|97|96)\d{7}/;
+    var valid = re.test(phone);
+   if (re.test(phone) == true )
+    	alert("Мы услышим Вас");
+    else 
+   alert("Мы вас не слышим");
+}
+   
