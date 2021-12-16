d406d5f7fe3f5147a5378288cc2038fff6645f23
Lebedieva2705 <lubashatsymbalist@gmail.com>
2021-12-16 02:44:26 +0200

    ДЗ15

---
+++ ДЗ15/src/LiubovFirst.java
@@ -0,0 +1,85 @@
+
+import java.util.Scanner;
+public class LiubovFirst {
+	public static void main(String[] args) {
+    /*Необходимо вывести на экран числа от 5 до 1. На экране должно быть:
+	 5 4 3 2 1*/
+
+		/*for (int i=5; i>=1; i--) {
+        System.out.print(i + " ");}
+}*/
+
+
+		/*Необходимо вывести на экран таблицу умножения на 3:
+		3*1=3
+		3*2=6
+		3*3=9
+		3*4=12
+		3*5=15
+		3*6=18
+		3*7=21
+		3*8=24
+		3*9=27
+		3*10=30*/
+
+        /*for (int i = 1; i <= 10; i++) {
+            System.out.println("3*" + i + "=" + 3 * i);}
+    }*/
+
+
+		/* Необходимо написать программу, где бы пользователю предлагалось ввести число на выбор:
+      1, 2 или 3, а программа должна сказать, какое число ввёл пользователь: 1, 2, или 3.
+      Написать двумя способами. If .
+      */
+		/*System.out.println("Введите число 1, 2 или 3: ");
+		Scanner inputFigure = new Scanner(System.in);
+		int i = inputFigure.nextInt();
+		System.out.println(i);
+		if (i == 1) {
+			System.out.println("Вы ввели число 1");
+		} else if (i == 2) {
+			System.out.println("Вы ввели число 2");
+		} else if (i == 3) {
+			System.out.println("Вы ввели число 3");
+		} else {
+			System.out.println("Вы ввели число неверное число");
+		}*/
+
+		/* Необходимо написать программу, где бы пользователю предлагалось ввести число на выбор:
+      1, 2 или 3, а программа должна сказать, какое число ввёл пользователь: 1, 2, или 3.
+      Написать двумя способами.  Switch.
+      */
+		/*System.out.println("Введите число 1, 2 или 3: ");
+		Scanner inputFigure = new Scanner(System.in);
+		int l = inputFigure.nextInt();
+		System.out.println(l);
+		switch (l) {
+			case 1:
+				System.out.println("Вы ввели число 1");
+				break;
+			case 2:
+				System.out.println("Вы ввели число 2");
+				break;
+			case 3:
+				System.out.println("Вы ввели число 3");
+				break;
+			default:
+				System.out.println("Вы ввели неверное число");
+		}
+	}
+}*/
+	/* Найти среднее арефметическое суммы чисел от 1 до 100 */
+
+		/*float sum = 0;
+		for (int i = 1; i <= 100; i++){
+			sum = sum + i;
+		}
+		System.out.println ("Среднее арифметические = " +(sum/100));*/
+
+		/*double sum = 0;
+		int average = 100;
+		for (int i = 1 ; i<= 100; i++){
+			sum += i;}
+			System.out.println((sum/average));*/
+		}
+	}
