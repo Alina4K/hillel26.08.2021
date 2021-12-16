44480c03b1a4e89a6449ec82f6ceef5080b8a9d7
YuriLinsky <djcarllinsky@gmail.com>
2021-12-16 13:23:19 +0200

    My Home Work 15

---
+++ src/HomeWork15.java
@@ -0,0 +1,91 @@
+import java.util.Scanner;
+
+public class HomeWork15 {
+
+        /* HOME WORK (Part I)
+        Необходимо написать программу, где бы пользователю предлагалось ввести число на выбор:
+      1, 2 или 3, а программа должна сказать, какое число ввёл пользователь: 1, 2, или 3.
+      Написать двумя способами. If и switch.
+      */
+
+//        public static void main(String[] args){
+//        System.out.println("Введите число 1, 2 или 3: ");
+//        Scanner inputFigure = new Scanner(System.in);
+//        int i = inputFigure.nextInt();
+//        if ( i==1) {
+//            System.out.println("Вы ввели число 1");
+//        }
+//        else if (i==2) {
+//            System.out.println ("Вы ввели число 2");
+//        }
+//        else if (i==3) {
+//            System.out.println ("Вы ввели число 3");
+//        }
+//        else {
+//            System.out.println ("Вы ввели неверное число");}
+//        System.out.println(i);
+
+
+//        public static void main(String[] args){
+//        System.out.println("Введите число 1, 2 или 3: ");
+//        Scanner inputFigure = new Scanner(System.in);
+//        int i = inputFigure.nextInt();
+//        switch (i){
+//            case 1:
+//                System.out.println ("Вы ввели число 1"); break;
+//            case 2:
+//                System.out.println ("Вы ввели число 2"); break;
+//            case 3:
+//                System.out.println ("Вы ввели число 3"); break;
+//            default:
+//                System.out.println("Вы ввели неверное число");}
+
+
+
+        /* HOME WORK (Part II)
+        Необходимо вывести на экран числа от 5 до 1. На экране должно быть:
+        5 4 3 2 1 */
+
+//    public static void main(String[] args) {
+//        for (int a = 5; a >= 1; a--) {
+//            System.out.print(a + " ");
+//        }
+
+
+
+       /* HOME WORK (Part III)
+       Необходимо вывести на экран таблицу умножения на 3:
+        3*1=3
+        3*2=6
+        3*3=9
+        3*4=12
+        3*5=15
+        3*6=18
+        3*7=21
+        3*8=24
+        3*9=27
+        3*10=30
+        */
+
+
+//      public static void main(String[] args){
+//         for (int a=1; a <=10; a++)
+//            System.out.println ("3 * " +a + " = " + 3*a);
+
+
+
+
+        // HOME WORK (Part IV)
+        // Найти среднее арефметическое суммы чисел от 1 до 100
+
+    
+//    public static void main(String[] args){
+//        float sum=0;
+//        for (int a=1; a<=100; a++){
+//            sum=sum+a;
+//        }
+//        System.out.println (+(sum/100));
+    }
+}
+
+
