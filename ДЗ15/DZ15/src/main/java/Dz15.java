887a4e94e4d9777ab77b7231f6bc4af052124a88
svyat-new <same92121@gmail.com>
2021-12-21 15:38:32 +0200

    INT

--- ДЗ15/DZ15/src/main/java/Dz15.java
+++ ДЗ15/DZ15/src/main/java/Dz15.java
@@ -2,41 +2,41 @@ import java.util.Scanner;
 
 public class Dz15 {
     //Необходимо вывести на экран таблицу умножения на 3:
-//    public static void main(String[] args) {
-//        for (int a = 1; a <= 10; a++)
-//            System.out.println("3 * " + a + " = " + 3 * a);
-//    }}
+    public static void main(String[] args) {
+        for (int a = 1; a <= 10; a++)
+            System.out.println("3 * " + a + " = " + 3 * a);
+    }}
 
 /* Необходимо вывести на экран числа от 5 до 1. На экране должно быть:
         5 4 3 2 1 */
 //
-//    public static void main(String[] args) {
-//        for (int a = 5; a >= 1; a--) {
-//            System.out.print(a + " ");
-//        }}}
+    public static void main(String[] args) {
+        for (int a = 5; a >= 1; a--) {
+            System.out.print(a + " ");
+        }}}
 
 //      Необходимо написать программу, где бы пользователю предлагалось ввести число на выбор:
 //      1, 2 или 3, а программа должна сказать, какое число ввёл пользователь: 1, 2, или 3.
 //      Cпособ If:
 
-//        public static void main (String [] args) {
-//
-//            System.out.print ("Введите число 1, 2 или 3: ");
-//            Scanner inputFigure = new Scanner (System.in);
-//            int i = inputFigure.nextInt ();
-//            if ( i==1) {
-//                System.out.println ("Вы ввели число 1");
-//            }
-//            else if (i==2) {
-//                System.out.println ("Вы ввели число 2");
-//            }
-//            else if (i==3) {
-//                System.out.println ("Вы ввели число 3");
-//            }
-//            else {
-//                System.out.println ("Вы ввели число не равное 1, 2 или 3");
-//            }
-//        }}
+        public static void main (String [] args) {
+
+            System.out.print ("Введите число 1, 2 или 3: ");
+            Scanner inputFigure = new Scanner (System.in);
+            int i = inputFigure.nextInt ();
+            if ( i==1) {
+                System.out.println ("Вы ввели число 1");
+            }
+            else if (i==2) {
+                System.out.println ("Вы ввели число 2");
+            }
+            else if (i==3) {
+                System.out.println ("Вы ввели число 3");
+            }
+            else {
+                System.out.println ("Вы ввели число не равное 1, 2 или 3");
+            }
+        }}
 
 //Необходимо написать программу, где бы пользователю предлагалось ввести число на выбор:
 ////      1, 2 или 3, а программа должна сказать, какое число ввёл пользователь: 1, 2, или 3.
--- ДЗ16/src/Main.java
+++ ДЗ16/src/Main.java
@@ -1,2 +1,11 @@
-public class Main {
+public class User1 {
+public String Name;
+    public String LastName;
+    public String Email;
+    public String Password;
+    public String RePassword;
+
+    public User1 (String Name, String LastName, String Email, String Password, String RePassword)
+        this.Name = Name;
+
 }
